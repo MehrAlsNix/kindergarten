affa6769a6afcf6d1fad3a1ed17c587e2115ce06
siad007 <siad.ardroumli@gmail.com>
2015-03-29 19:23:14 +0200

    Added vendor autoload

--- bin/kindergarten.php
+++ bin/kindergarten.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__ . '/../src/Application.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 $app = new siad007\kindergarten\Application();
 $app->run();
