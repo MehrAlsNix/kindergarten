92fede9a4f383f85b2dc0e8b1ba09d34c32a56d2
siad007 <siad.ardroumli@gmail.com>
2015-03-29 19:58:46 +0200

    Prepared logging and configuration

--- src/Application.php
+++ src/Application.php
@@ -3,6 +3,14 @@
 namespace siad007\kindergarten;
 
 use Cilex\Application as Cilex;
+use Cilex\Provider\MonologServiceProvider;
+use Monolog\ErrorHandler;
+use Monolog\Handler\NullHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Shell;
+use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Stopwatch\Stopwatch;
 
 class Application extends Cilex
@@ -17,5 +25,145 @@ class Application extends Cilex
         $this['kernel.stopwatch'] = function () {
             return new Stopwatch();
         };
+
+        // $this->addLogging();
+        // $this->addConfiguration();
+    }
+
+    /**
+     * Run the application and if no command is provided, use project:run.
+     *
+     * @param bool $interactive Whether to run in interactive mode.
+     *
+     * @return void
+     */
+    public function run($interactive = false)
+    {
+        /** @var ConsoleApplication $app */
+        $app = $this['console'];
+        $app->setAutoExit(false);
+        if ($interactive) {
+            $app = new Shell($app);
+        }
+        $output = new Console\Output\Output();
+        // $output->setLogger($this['monolog']);
+        $app->run(new ArgvInput(), $output);
+    }
+
+    /**
+     * Adds a logging provider to the container.
+     *
+     * @return void
+     */
+    protected function addLogging()
+    {
+        $this->register(
+            new MonologServiceProvider(),
+            array(
+                'monolog.name' => 'kindergarten',
+                'monolog.logfile' => sys_get_temp_dir() . '/kindergarten.log',
+                'monolog.debugfile' => sys_get_temp_dir() . '/kindergarten.debug.log',
+                'monolog.level' => Logger::INFO,
+            )
+        );
+        $app = $this;
+        $this['monolog.configure'] = $this->protect(
+            function ($log) use ($app) {
+                $level = (string)$app['config']->logging->level;
+
+                $logPath = isset($app['config']->logging->paths->default)
+                    ? (string)$app['config']->logging->paths->default
+                    : null;
+
+                $debugPath = isset($app['config']->logging->paths->errors)
+                    ? (string)$app['config']->logging->paths->errors
+                    : null;
+                $app->configureLogger($log, $level, $logPath, $debugPath);
+            }
+        );
+        ErrorHandler::register($this['monolog']);
+    }
+
+    public function configureLogger($logger, $level, $logPath = null)
+    {
+        /** @var Logger $monolog */
+        $monolog = $logger;
+        switch ($level) {
+            case 'emergency':
+            case 'emerg':
+                $level = Logger::EMERGENCY;
+                break;
+            case 'alert':
+                $level = Logger::ALERT;
+                break;
+            case 'critical':
+            case 'crit':
+                $level = Logger::CRITICAL;
+                break;
+            case 'error':
+            case 'err':
+                $level = Logger::ERROR;
+                break;
+            case 'warning':
+            case 'warn':
+                $level = Logger::WARNING;
+                break;
+            case 'notice':
+                $level = Logger::NOTICE;
+                break;
+            case 'info':
+                $level = Logger::INFO;
+                break;
+            case 'debug':
+                $level = Logger::DEBUG;
+                break;
+        }
+        $this['monolog.level'] = $level;
+        if ($logPath) {
+            $logPath = str_replace(
+                array('{APP_ROOT}', '{DATE}'),
+                array(realpath(__DIR__ . '/../..'), $this['kernel.timer.start']),
+                $logPath
+            );
+            $this['monolog.logfile'] = $logPath;
+        }
+// remove all handlers from the stack
+        try {
+            while ($monolog->popHandler()) {
+            }
+        } catch (\LogicException $e) {
+// popHandler throws an exception when you try to pop the empty stack; to us this is not an
+// error but an indication that the handler stack is empty.
+        }
+        if ($level === 'quiet') {
+            $monolog->pushHandler(new NullHandler());
+            return;
+        }
+// set our new handlers
+        if ($logPath) {
+            $monolog->pushHandler(new StreamHandler($logPath, $level));
+        } else {
+            $monolog->pushHandler(new StreamHandler('php://stdout', $level));
+        }
+    }
+
+    /**
+     * Adds the Configuration object to the DIC.
+     *
+     * @return void
+     */
+    protected function addConfiguration()
+    {
+        $this['config'] = $this->share(
+            function () {
+                $user_config_file = (file_exists(getcwd() . DIRECTORY_SEPARATOR . 'phpdoc.xml'))
+                    ? getcwd() . DIRECTORY_SEPARATOR . 'phpdoc.xml'
+                    : getcwd() . DIRECTORY_SEPARATOR . 'phpdoc.dist.xml';
+                if (is_readable($user_config_file)) {
+                    $config_files[] = $user_config_file;
+                }
+                return []; // Factory::fromFiles($config_files, true);
+            }
+        );
     }
 }
