74b3d76a305584df508a282ea85d9f0d49eeac97
siad007 <siad.ardroumli@gmail.com>
2015-04-06 16:46:31 +0200

    Added register implementation spec

--- spec/Analyzer/ServiceProviderSpec.php
+++ spec/Analyzer/ServiceProviderSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\MehrAlsNix\kindergarten\Analyzer;
 
+use Cilex\Application;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -12,8 +13,9 @@ class ServiceProviderSpec extends ObjectBehavior
         $this->shouldHaveType('MehrAlsNix\kindergarten\Analyzer\ServiceProvider');
     }
 
-    function it_should_be_a_service_provider()
+    function it_should_be_a_service_provider(Application $app)
     {
         $this->shouldImplement('Cilex\ServiceProviderInterface');
+        $this->register($app)->shouldReturn(null);
     }
 }
--- spec/Comparator/ServiceProviderSpec.php
+++ spec/Comparator/ServiceProviderSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\MehrAlsNix\kindergarten\Comparator;
 
+use Cilex\Application;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -12,8 +13,9 @@ class ServiceProviderSpec extends ObjectBehavior
         $this->shouldHaveType('MehrAlsNix\kindergarten\Comparator\ServiceProvider');
     }
 
-    function it_should_be_a_service_provider()
+    function it_should_be_a_service_provider(Application $app)
     {
         $this->shouldImplement('Cilex\ServiceProviderInterface');
+        $this->register($app)->shouldReturn(null);
     }
 }
